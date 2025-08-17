bfd7c9fe03d87b4e71b6c92f92bce85b61a68c35
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2025-08-17 11:33:53 +0800

    web controller update for loading screen

--- src/controller/WebController.jsx
+++ src/controller/WebController.jsx
@@ -1,11 +1,34 @@
 import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Home from "../components/Home";
 import About from "../components/About";
 import Navbar from "../components/Navbar";
 import Pastries from "../components/Pastries";
 import Cart from "../components/Cart";
+import LoadingScreen from "../components/LoadingScreen";
+import '../styles/CommonStyles.css';
 
 function WebController() {
+  const [loading, setLoading] = useState(true);
+  const [fadeOut, setFadeOut] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setFadeOut(true); // start fade-out
+      setTimeout(() => setLoading(false), 1000); // remove after 1s
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className={`loading-wrapper ${fadeOut ? 'fade' : ''}`}>
+        <LoadingScreen />
+      </div>
+    );
+  }
+
   return (
     <>
       <Navbar />
