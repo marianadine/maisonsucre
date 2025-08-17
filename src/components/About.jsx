358385c6cc851d2741b5069347d8841f56e6c31f
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2025-08-17 15:38:56 +0800

    about ui update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -28,6 +28,17 @@ const About = () => {
       </section>
 
       <section className='container'>
+        <div className='row-container'>
+          <img className='mockup' src={honeycomb} alt="Maison Sucré" />
+          <div style={{ width: '60%' }} className='text-container'>
+            <h1 style={{ textAlign: 'left', marginBottom: '-10px' }} className='poppins-header abouts2'>Wrapped in elegance, sealed with care.</h1>
+
+            <p style={{ textAlign: 'justify' }} className='poppins-text abouts2'>At Maison Sucré, we handle our pastries with the same care we put into baking them. Every creation is placed in thoughtfully designed packaging that preserves freshness, protects delicate layers, and reflects our signature elegance. From the buttery flake of a croissant to the silky cream of a tiramisu, each piece is wrapped like a gift — because every bite should feel special before you even taste it.</p>
+          </div>
+        </div>
+      </section>
+
+      <section className='container'>
         <h1 className='poppins-header inquiry-header'>Contact Us</h1>
         <div className="inquiry-row">
           <p className="inquiry-text">
