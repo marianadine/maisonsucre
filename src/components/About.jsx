804ad696bed6462e710c3fdd50e2905666464552
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2025-08-17 11:44:09 +0800

    about section 1

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import honeycomb from '../imgs/honeycomb.jpg'
+import '../styles/About.css'
 
 const About = () => {
   return (
     <div>
-      
+      <section className="container">
+        <div className='row-container'>
+          <div className='text-container'>
+            <h1 className='mainpagetext' style={{ fontWeight: '600' }}>Maison Sucré</h1>
+            <p className='poppins-text' style={{ fontWeight: '600', fontStyle: 'italic' }}>/ meh-ZON soo-CRAY /</p>
+            <p className='poppins-text'>— Sweet House or House of Sweets</p>
+            <p className='poppins-text'>— A refined place where artisanal pastries and desserts are crafted with elegance and care. </p>
+          </div>
+          <img className='honeycomb' src={honeycomb} alt="Maison Sucré" />
+        </div>
+      </section>
     </div>
   )
 }
