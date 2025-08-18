aa855804565bdcfcf088124f468181ef65202df2
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2025-08-18 19:43:24 +0800

    navbar mobile responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import '../styles/Navbar.css';
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <nav className='navbar'>
-            <ul className='navbar-links'>
-                <li>
-                    <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/about" className={({ isActive }) => isActive ? "active" : ""}>about</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/pastry" className={({ isActive }) => isActive ? "active" : ""}>pastries</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/cart" className={({ isActive }) => isActive ? "active" : ""}>cart</NavLink>
-                </li>
-            </ul>
+            {/* right side container */}
+            <div className="nav-right">
+                <ul className={`navbar-links ${menuOpen ? "open" : ""}`}>
+                    <li>
+                        <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/about" className={({ isActive }) => isActive ? "active" : ""}>about</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/pastry" className={({ isActive }) => isActive ? "active" : ""}>pastries</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/cart" className={({ isActive }) => isActive ? "active" : ""}>cart</NavLink>
+                    </li>
+                </ul>
+
+                {/* Hamburger */}
+                <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                </div>
+            </div>
         </nav>
     );
 };
