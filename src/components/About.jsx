71e429fae6dd33321705941d68fc462cbbfce009
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2025-08-18 11:42:41 +0800

    text container styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,10 +16,10 @@ const About = () => {
         <div className='row-container aboutpage'>
           <img className='honeycomb' src={honeycomb} alt="Maison Sucré" />
           <div className='text-container'>
-            <h1 className='mainpagetext' style={{ fontWeight: '600' }}>Maison Sucré</h1>
-            <p className='poppins-text' style={{ fontWeight: '600', fontStyle: 'italic' }}>/ meh-ZON soo-CRAY /</p>
-            <p className='poppins-text'>— Sweet House or House of Sweets</p>
-            <p className='poppins-text'>— A refined place where artisanal pastries and desserts are crafted with elegance and care. </p>
+            <h1 className='mainpagetext' style={{ fontWeight: '600', color: '#fff', marginTop: '20px' }}>Maison Sucré</h1>
+            <p className='poppins-text' style={{ fontWeight: '600', fontStyle: 'italic', color: '#fff' }}>/ meh-ZON soo-CRAY /</p>
+            <p className='poppins-text' style={{ color: '#fff' }}>— Sweet House or House of Sweets</p>
+            <p className='poppins-text' style={{ color: '#fff' }}>— A refined place where artisanal pastries and desserts are crafted with elegance and care. </p>
           </div>
         </div>
       </section>
