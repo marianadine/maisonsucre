82162396389d3b5429b6c3eed5f7eae2e403d4d9
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2025-08-24 22:25:23 +0800

    add cart context

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useState, useContext } from "react";
+
+const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (product) => {
+    setCartItems((prev) => {
+      const existing = prev.find((item) => item.id === product.id);
+
+      if (existing) {
+        return prev.map((item) =>
+          item.id === product.id
+            ? { ...item, qty: item.qty + 1 }
+            : item
+        );
+      } else {
+        return [...prev, { ...product, qty: 1 }];
+      }
+    });
+  };
+
+  const deleteItem = (id) => {
+    setCartItems((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  return (
+    <CartContext.Provider value={{ cartItems, addToCart, deleteItem }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => useContext(CartContext);
